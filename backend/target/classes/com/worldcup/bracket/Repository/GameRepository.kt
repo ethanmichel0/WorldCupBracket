3137ac122384b160e17b9dcdcb8b8a845f3fbe2a
Ethan Michel <emichel2@oberlin.edu>
2022-09-24 18:17:10 +0200

    rename name to id for clarification

--- backend/target/classes/com/worldcup/bracket/Repository/GameRepository.kt
+++ backend/target/classes/com/worldcup/bracket/Repository/GameRepository.kt
@@ -11,10 +11,10 @@ interface GameRepository : MongoRepository<Game,String>{
     fun findByOrderByDateAsc() : List<Game>
 
     @Query("{\$and :[{\$or:[{'home._id': ?0 }, { 'away._id' : ?0}]},{'date': {\$gte: ?1}}]}")
-    fun getAllGamesFromTeamUpcoming(teamName: String, date: Long) : List<Game>
+    fun getAllGamesFromTeamUpcoming(teamId: String, date: Long) : List<Game>
 
     @Query("{\$and :[{\$or:[{'home._id': ?0 }, { 'away._id' : ?0}]},{'date': {\$lt: ?1}}]}")
-    fun getAllGamesFromTeamPast(teamName: String, date: Long) : List<Game>
+    fun getAllGamesFromTeamPast(teamId: String, date: Long) : List<Game>
 
     @Query("{\$or :[{\$and:[{'home.name': ?0 }, { 'away.name' : ?1}]},{\$and:[{'home.name': ?1 }, { 'away.name' : ?0}]}]}")
     fun getAllGamesBetweenTwoTeams(team1Name: String, team2Name: String) : List<Game>
